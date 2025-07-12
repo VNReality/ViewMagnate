ebe1d1c5ea188e2f3efddd446d3b88c4320e13d7
VNReality <viveknprofessional@gmail.com>
2025-07-12 08:08:47 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,18 @@
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('viewmagnate-cache').then(cache => {
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('viewmagnate-store').then(function(cache) {
       return cache.addAll([
-        './index.html',
-        './manifest.json',
-        './icon-192.png',
-        './icon-512.png'
+        '/',
+        '/index.html'
       ]);
     })
   );
 });
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
     })
   );
 });
